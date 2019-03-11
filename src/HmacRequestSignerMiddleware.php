1f5069e80625d6293a47490b502e61b82513cca6
Dominik Voda <d.voda94@gmail.com>
2019-03-11 11:07:45 +0100

    ion -> or

--- src/HmacRequestSignerMiddleware.php
+++ src/HmacRequestSignerMiddleware.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace BE\HmacRequestValidation;
+namespace BE\HmacRequestValidator;
 
 use Acquia\Hmac\Guzzle\HmacAuthMiddleware;
 use Acquia\Hmac\Key;
--- src/SignedHttpClientFactory.php
+++ src/SignedHttpClientFactory.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types = 1);
 
-namespace CustomComponents;
+namespace BE\HmacRequestValidator;
 
 use Acquia\Hmac\Guzzle\HmacAuthMiddleware;
 use GuzzleHttp\Client;
